738d0d85ebccaf12f0d0a0525c7e8c06325def16
Jairo Lopez <jairo@ruri.waseda.jp>
2014-05-19 18:57:24 +0900

    Modifications to run-sim.sh and Zhu's scenario
    The new script is a lot more verbose about things, making it easier to use,
    hopefully. Zhu's scenarios has been modified so that it works with the scripts.
    I will now run everything tonight.
    
    Cheers.

--- ndn-campus/run-sim.sh
+++ ndn-campus/run-sim.sh
@@ -15,17 +15,22 @@ XFLAG=0
 DFLAG="results"
 
 function usage() {
-echo "Tiny script to automize running of a ns3 scenario"
-echo "Options:"
-echo "    -c NUM    Number of clients (consumers) for the scenario. Default [$CFLAG]"
-echo "    -n NUM    Number of networks the scenario will have. Default [$NFLAG]"
-echo "    -p NUM    Number of servers (producers) for the scenario. Default [$PFLAG]"
-echo "    -r NUM    Number of times to run the scenario. Default [$RFLAG]"
-echo "    -s NUM    Size, in MB of the content to be distributed. Default [$SFLAG]"
-echo "    -t        Run TCP scenario. Default [$TFLAG]"
-echo "    -x        Run NDN scenario. Default [$XFLAG]"
-echo "    -d DIR    Directory to place the results. Default [$DFLAG]"
-echo ""
+    echo "Tiny script to automize running of a ns3 scenario"
+    echo "Options:"
+    echo "    -c NUM    Number of clients (consumers) for the scenario. Default [$CFLAG]"
+    echo "    -n NUM    Number of networks the scenario will have. Default [$NFLAG]"
+    echo "    -p NUM    Number of servers (producers) for the scenario. Default [$PFLAG]"
+    echo "    -r NUM    Number of times to run the scenario. Default [$RFLAG]"
+    echo "    -s NUM    Size, in MB of the content to be distributed. Default [$SFLAG]"
+    echo "    -t        Run TCP scenario. Default [$TFLAG]"
+    echo "    -x        Run NDN scenario. Default [$XFLAG]"
+    echo "    -d DIR    Directory to place the results. Default [$DFLAG]"
+    echo ""
+}
+
+function checkRun() {
+    echo "Checking for $1"
+    $WAF list 2>&1 > /dev/null | grep $1 2>&1 > /dev/null
 }
 
 while getopts "r:d:c:s:n:p:htx" OPT
@@ -81,8 +86,20 @@ if [ ! -x $CONTSIZE ]; then
     cd $WAF
 fi
 
+if [ ! -e $WAFDIR/sims.conf ]; then
+    echo "$WAFDIR/sims.conf does not exist! Must be created to run the simulations!"
+    exit 1
+fi
+
+source $WAFDIR/sims.conf
+
 for i in $(seq 1 $RFLAG)
 do
+
+    if [ $TFLAG -eq 0 ] && [ $XFLAG -eq 0 ]; then
+        echo "No -t or -x flag set! No simulations to run!"
+        exit 0
+    fi
 
     BYTES=$($CONTSIZE --avg $SFLAG)
 
@@ -97,10 +114,32 @@ do
     $PRINTF "Saving in %s\n" $RUN
 
     if [ $TFLAG -eq 1 ]; then
-        $WAF --run "disaster-tcp --clients=$CFLAG --contentsize=$BYTES --networks=$NFLAG --servers=$PFLAG  --results=$RUN"
+        if [ -z $TCPSIM ]; then
+            echo "$WAFDIR/sims.conf does not declare \$TCPSIM!"
+            exit 1
+        fi
+
+        checkRun $TCPSIM
+        if [ $? -ne 0 ]; then
+            echo "No scenario $TCPSIM!"
+            exit 1
+        fi
+
+        $WAF --run "$TCPSIM --clients=$CFLAG --contentsize=$BYTES --networks=$NFLAG --servers=$PFLAG  --results=$RUN"
     fi
 
     if [ $XFLAG -eq 1 ]; then
-        $WAF --run "nms-disaster-ccn --clients=$CFLAG --contentsize=$BYTES --networks=$NFLAG --servers=$PFLAG  --results=$RUN"
+        if [ -z $NDNSIM ]; then
+            echo "$WAFDIR/sims.conf does not declare \$NDNSIM!"
+            exit 1
+        fi
+
+        checkRun $NDNSIM
+        if [ $? -ne 0 ]; then
+            echo "No scenario $NDNSIM!"
+            exit 1
+        fi
+
+        $WAF --run "$NDNSIM --clients=$CFLAG --contentsize=$BYTES --networks=$NFLAG --servers=$PFLAG  --results=$RUN"
     fi
 done
--- ndn-campus/scenarios/nms-disaster-ccn_zl.cc
+++ ndn-campus/scenarios/nms-disaster-ccn_zl.cc
@@ -91,8 +91,8 @@ br::mt19937_64 gen;
 // Obtains a random number from a uniform distribution between min and max.
 // Must seed number generator to ensure randomness at runtime.
 int obtain_Num(int min, int max) {
-    br::uniform_int_distribution<> dist(min, max);
-    return dist(gen);
+	br::uniform_int_distribution<> dist(min, max);
+	return dist(gen);
 }
 
 // Obtains a random list of clients and servers. Must be run once all nodes have been
@@ -165,10 +165,10 @@ class Array2D
 {
 public:
 	Array2D (const size_t x, const size_t y) : p (new T*[x]), m_xMax (x)
-	{
+{
 		for (size_t i = 0; i < m_xMax; i++)
 			p[i] = new T[y];
-	}
+}
 
 	~Array2D (void)
 	{
@@ -193,10 +193,10 @@ class Array3D
 {
 public:
 	Array3D (const size_t x, const size_t y, const size_t z) : p (new Array2D<T>*[x]), m_xMax (x)
-	{
+{
 		for (size_t i = 0; i < m_xMax; i++)
 			p[i] = new Array2D<T> (y, z);
-	}
+}
 
 	~Array3D (void)
 	{
@@ -228,7 +228,7 @@ int main (int argc, char *argv[])
 
 	int nCN = 3, nLANClients = 42;
 	bool nix = true;
-	
+
 	// Char array for output strings
 	char buffer[250];
 
@@ -237,16 +237,18 @@ int main (int argc, char *argv[])
 	uint32_t clients = 1; // Number of clients in the network
 	uint32_t servers = 1; // Number of servers in the network
 	uint32_t networks = 1; // Number of additional nodes in the network
+	char results[250] = "results";
 
 	CommandLine cmd;
 	cmd.AddValue ("CN", "Number of total CNs [2]", nCN);
 	cmd.AddValue ("LAN", "Number of nodes per LAN [42]", nLANClients);
 	cmd.AddValue ("NIX", "Toggle nix-vector routing", nix);
 	cmd.AddValue ("contentsize",
-				"Total number of bytes for application to send", contentsize);
+			"Total number of bytes for application to send", contentsize);
 	cmd.AddValue ("clients", "Total number of clients in the network", clients);
 	cmd.AddValue ("servers", "Total number of servers in the network", servers);
 	cmd.AddValue ("networks", "Number of networks in the simulation", networks);
+	cmd.AddValue ("results", "Directory to place results", results);
 	cmd.Parse (argc,argv);
 	/*if (nCN < 2)
 	{
@@ -254,12 +256,12 @@ int main (int argc, char *argv[])
 				<< std::endl;
 		return 1;
 	}*/
-    
-		
+
+
 	// Overwrite nCN with networks
-    nCN = networks;
-	
-    std::cout << "Number of CNs: " << nCN << ", LAN nodes: " << nLANClients << std::endl;
+	nCN = networks;
+
+	std::cout << "Number of CNs: " << nCN << ", LAN nodes: " << nLANClients << std::endl;
 
 	Array2D<NodeContainer> nodes_net0(nCN, 3);
 	Array2D<NodeContainer> nodes_net1(nCN, 6);
@@ -566,7 +568,7 @@ int main (int argc, char *argv[])
 
 	// Make sure to seed our random
 	gen.seed(std::time(0));
-	
+
 	// With the network assigned, time to randomly obtain clients and servers
 	NS_LOG_INFO ("Obtaining the clients and servers");
 	// Obtain the random lists of server and clients
@@ -580,37 +582,37 @@ int main (int argc, char *argv[])
 	std::vector<uint32_t> clientNodeIds;
 	NodeContainer serverNodes;
 	std::vector<uint32_t> serverNodeIds;
-	
+
 	// We have to manually introduce the Ptr<Node> to the NodeContainers
-		// We do this to make them easier to control later
-		for (uint32_t i = 0; i < clients ; i++)
-		{
-			Ptr<Node> tmp = clientVector[i];
+	// We do this to make them easier to control later
+	for (uint32_t i = 0; i < clients ; i++)
+	{
+		Ptr<Node> tmp = clientVector[i];
 
-			uint32_t nodeNum = tmp->GetId();
+		uint32_t nodeNum = tmp->GetId();
 
-			sprintf (buffer, "Adding client node: %d", nodeNum);
-			NS_LOG_INFO (buffer);
+		sprintf (buffer, "Adding client node: %d", nodeNum);
+		NS_LOG_INFO (buffer);
 
-			clientNodes.Add(tmp);
-			clientNodeIds.push_back(nodeNum);
-		}
+		clientNodes.Add(tmp);
+		clientNodeIds.push_back(nodeNum);
+	}
 
-		// Do the same for the server NodeContainer
-		for (uint32_t i = 0; i < servers; i++)
-		{
-			Ptr<Node> tmp = serverVector[i];
+	// Do the same for the server NodeContainer
+	for (uint32_t i = 0; i < servers; i++)
+	{
+		Ptr<Node> tmp = serverVector[i];
 
-			uint32_t nodeNum = tmp->GetId();
+		uint32_t nodeNum = tmp->GetId();
 
-			sprintf (buffer, "Adding server node: %d", nodeNum);
-			NS_LOG_INFO (buffer);
+		sprintf (buffer, "Adding server node: %d", nodeNum);
+		NS_LOG_INFO (buffer);
 
-			serverNodes.Add(tmp);
-			serverNodeIds.push_back(nodeNum);
-		}
+		serverNodes.Add(tmp);
+		serverNodeIds.push_back(nodeNum);
+	}
 
-	
+
 	/*// Create Traffic Flows
 	std::cout << "Creating TCP Traffic Flows:" << std::endl;
 	Config::SetDefault ("ns3::OnOffApplication::MaxBytes", UintegerValue (500000));
@@ -719,17 +721,21 @@ int main (int argc, char *argv[])
 	delete[] nodes_netLR;*/
 
 	// Calculate routing tables
-	
-		//std::cout << "Populating Global Static Routing Tables..." << std::endl;
+
+	//std::cout << "Populating Global Static Routing Tables..." << std::endl;
 	//Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
 	ndn::StackHelper ndnHelper;
 	//ndnHelper.SetDefaultRoutes (true);
+	// Install Content Store
+	ndnHelper.SetContentStore("ns3::ndn::cs::Lru","MaxSize","10000");
 	ndnHelper.InstallAll ();
 
 	ndn::GlobalRoutingHelper ndnGlobalRoutingHelper;
-		ndnGlobalRoutingHelper.InstallAll ();
-		ndnGlobalRoutingHelper.AddOrigins ("/Dinfo/tokyo/shinjuku/waseda-u/waseda", serverNodes);
-		ndn::GlobalRoutingHelper::CalculateRoutes ();
+	ndnGlobalRoutingHelper.InstallAll ();
+	ndnGlobalRoutingHelper.AddOrigins ("/Dinfo/tokyo/shinjuku/waseda-u/waseda", serverNodes);
+	ndn::GlobalRoutingHelper::CalculateRoutes ();
+
+
 
 	// Consumer
 	ndn::AppHelper consumerHelper ("ns3::ndn::ConsumerCbr");
@@ -737,7 +743,13 @@ int main (int argc, char *argv[])
 	consumerHelper.SetPrefix ("/Dinfo/tokyo/shinjuku/waseda-u/waseda");
 	consumerHelper.SetAttribute ("Frequency", StringValue ("100")); // 10 interests a second
 	//consumerHelper.Install (nodes.Get (12)); // first node
-	consumerHelper.Install (clientNodes);
+	//consumerHelper.Install (clientNodes);
+
+	ApplicationContainer apps;
+
+	apps = consumerHelper.Install (clientNodes);
+	apps.Start (Seconds (0.1));
+	apps.Stop (Seconds (15.0)); 
 
 
 	// Producer
@@ -745,15 +757,52 @@ int main (int argc, char *argv[])
 	// Producer will reply to all requests starting with /prefix
 	producerHelper.SetPrefix ("/Dinfo/tokyo/shinjuku/waseda-u/waseda");
 	producerHelper.SetAttribute ("PayloadSize", StringValue("1024"));
+
+	producerHelper.SetAttribute ("Freshness", TimeValue (Seconds(0)));
 	//producerHelper.Install (nodes.Get (2)); // last node
 	producerHelper.Install (serverNodes);
 
-	// Obtain metrics
-	ndn::L3AggregateTracer::InstallAll("results/disaster-ccn-aggregate-trace.txt", Seconds (1.0));
-	ndn::L3RateTracer::InstallAll ("results/disaster-ccn-rate-trace.txt", Seconds (1.0));
-	ndn::AppDelayTracer::InstallAll ("results/disaster-ccn-app-delays-trace.txt");
-	L2RateTracer::InstallAll ("results/disaster-ccn-drop-trace.txt", Seconds (0.5));
 
+	// Obtain metrics
+	char filename[250];
+
+	// Print server nodes to file
+	sprintf(filename, "%s/disaster-ccn-servers-%02d-%03d-%03d-%0*d.txt", results, networks, servers, clients, 12, contentsize);
+
+	NS_LOG_INFO ("Printing node files");
+	std::ofstream serverFile;
+	serverFile.open (filename);
+	for (int i = 0; i < serverNodeIds.size(); i++) {
+		serverFile << serverNodeIds[i] << std::endl;
+	}
+	serverFile.close();
+
+	sprintf(filename, "%s/disaster-ccn-clients-%02d-%03d-%03d-%0*d.txt", results, networks, servers, clients, 12, contentsize);
+
+	std::ofstream clientFile;
+	clientFile.open (filename);
+	for (int i = 0; i < clientNodeIds.size(); i++) {
+		clientFile << clientNodeIds[i] << std::endl;
+	}
+	clientFile.close();
+
+	sprintf (filename, "%s/disaster-ccn-aggregate-trace-%02d-%03d-%03d-%0*d.txt", results, networks, servers, clients, 12, contentsize);
+	ndn::L3AggregateTracer::InstallAll(filename, Seconds (1.0));
+
+	sprintf (filename, "%s/disaster-ccn-rate-trace-%02d-%03d-%03d-%0*d.txt", results, networks, servers, clients, 12, contentsize);
+	ndn::L3RateTracer::InstallAll (filename, Seconds (1.0));
+
+	sprintf (filename, "%s/disaster-ccn-app-delays-trace-%02d-%03d-%03d-%0*d.txt", results, networks, servers, clients, 12, contentsize);
+	ndn::AppDelayTracer::InstallAll (filename);
+
+	sprintf (filename, "%s/disaster-ccn-drop-trace-%02d-%03d-%03d-%0*d.txt", results, networks, servers, clients, 12, contentsize);
+	L2RateTracer::InstallAll (filename, Seconds (0.5));
+
+	sprintf (filename, "%s/disaster-ccn-cs-trace-%02d-%03d-%03d-%0*d.txt", results, networks, servers, clients, 12, contentsize);
+	ndn::CsTracer::InstallAll (filename, Seconds (1));
+
+	p2p_1gb5ms.EnablePcap ("results/ccn_test0.pcap", serverNodes.Get(0)->GetId (), true,true);
+	//p2p_1gb5ms.EnablePcap ("results/tcp_test1.pcap", serverNodes.Get(1)->GetId (), true,true);
 	Simulator::Stop (Seconds (20.0));
 
 	Simulator::Run ();
