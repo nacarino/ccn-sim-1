4373f10ea049a5a87a1043b2514ad820d6834a96
Jairo Eduardo Lopez <jelfn@sislacom.com>
2014-05-25 23:09:08 +0900

    Content Store tracer graphing, updates on rest
    The new cs-tr-j.R takes care of graphing the Content Store hit rates for NDN
    simulations.
    
    l2-tr-j.R has been modified use subset instead of the array notation.
    
    nms-disaster-ccn_zl.cc was modified to test out the content store trace. At
    the moment it asks for two interests, at two different time intervals. This
    is a slightly different scenario.
    
    Will have to speak to the team about how to populate the content stores
    randomly.
    
    Cheers.

---
+++ ndn-campus/graphs/cs-tr-j.R
@@ -0,0 +1,143 @@
+#!/usr/bin/Rscript
+
+# Simple R script to make graphs from ndnSIM tracers - Content Store trace
+# 2014-05-23
+# Jairo Eduardo Lopez
+
+# Load packages but supress output
+suppressPackageStartupMessages(library (ggplot2))
+suppressPackageStartupMessages(library (scales))
+suppressPackageStartupMessages(library (optparse))
+suppressPackageStartupMessages(library (doBy))
+
+# set some reasonable defaults for the options that are needed
+option_list <- list (
+  make_option(c("-p", "--producers"), type="integer", default=1,
+              help="Number of servers (producers) which will be displayed on\n\t\tthe graph title."),
+  make_option(c("-c", "--clients"), type="integer", default=1,
+              help="Number of the clients (consumers) which will be displayed\n\t\ton the graph title."),
+  make_option(c("-n", "--networks"), type="integer", default=1,
+              help="Number of networks which will be displayed on the graph title."),
+  make_option(c("-f", "--file"), type="character", default="results/cs-trace.txt",
+              help="File which holds the raw content store data.\n\t\t[Default \"%default\"]"),
+  make_option(c("-e", "--node"), type="integer", default=-1,
+              help="Node data to graph. Default graphs all"),
+  make_option(c("-o", "--output"), type="character", default=".",
+              help="Output directory for graphs.\n\t\t[Default \"%default\"]")
+  )
+
+# Load the parser
+opt = parse_args(OptionParser(option_list=option_list, description="Creates graphs from ndnSIM Content Store Tracer data"))
+
+data = read.table (opt$file, header=T)
+data$Node = factor (data$Node)
+data$Type = factor (data$Type)
+
+sel = c()
+
+times = factor(data$Time)
+
+timevals = levels(times)
+
+nTimes = length(timevals)[1]
+nNodes = length(levels(data$Node))[1]
+
+N = nTimes*nNodes
+
+df = data.frame(Time=rep(NA, N), Node=rep(NA, N), Hit=rep(NA,N), Miss=rep(NA,N), Ratio=rep(NA,N))
+
+pos = 0
+
+for (i in 1:nTimes) {
+  iset = subset(data, Time %in% timevals[i])
+  
+  for (j in 0:(nNodes-1)) {
+    
+    jset = subset(iset, Node %in% j)
+    
+    hits = subset(jset, Type %in% "CacheHits")
+    misses = subset(jset,  Type %in% "CacheMisses")
+    
+    h = hits$Packets
+    m = misses$Packets
+    denom = h + m
+    
+    res = 0
+    if (denom > 0) {
+      res = h / denom
+    }
+    
+    df[pos,] = c(i, j, h, m, res)
+    pos = pos + 1
+  }
+}
+
+
+# Check that there are Hit or Miss numbers
+for (i in 0:(nNodes-1)) {
+  add = FALSE
+  if (mean(df[df$Node %in% i,]$Hit) > 0) {
+    add = TRUE
+  }
+  if (mean(df[df$Node %in% i,]$Miss) > 0) {
+    add = TRUE
+  }
+  
+  if (add) {
+    sel = append(sel, i)
+  }
+}
+
+
+if (length(sel) != 0) {
+  name = ""
+  
+  df$Node = factor (df$Node)
+  
+  if (opt$node >= 0) {
+    df = subset (df, Node %in% opt$node)
+    
+    if (dim(data)[1] == 0) {
+      cat(sprintf("There is no Node %d in this trace!", opt$node))
+      quit("yes")
+    }
+    name = sprintf("Cache Hits on Node %d of Campus Network, %d campuses, %d server, %d client",
+                   opt$node, opt$networks, opt$producers, opt$clients)
+  } else {
+    # Filter with sel
+    df = subset(df, Node %in% sel)
+    
+    name = sprintf("Cache Hits for Campus Network, %d campuses, %d server, %d client",
+                   opt$networks, opt$producers, opt$clients)
+  }
+  
+  # graph cache hit rates on all nodes
+  g.all <- ggplot (df, aes(colour=Action)) +
+    geom_line(aes (x=Time, y=Hit, colour="Hit")) + 
+    geom_line(aes (x=Time, y=Miss, colour="Miss")) +
+    ylab ("Cache Hit/Miss Rate") +
+    ggtitle (name) +
+    facet_wrap (~Node) +
+    theme_bw ()
+  
+  # Get the basename of the file
+  tmpname = strsplit(opt$file, "/")[[1]]
+  filename = tmpname[length(tmpname)]
+  # Get rid of the extension
+  noext = gsub("\\..*", "", filename)
+  
+  # The output png
+  if (opt$node > -1) {
+    outpng = sprintf("%s/%s-%d.png", opt$output, noext, opt$node)
+  } else {
+    outpng = sprintf("%s/%s.png", opt$output, noext)
+  }
+  
+  png (outpng, width=1024, height=768)
+  print (g.all)
+  x = dev.off ()
+} else {
+  cat("There are no hits or misses in Cache in this simulation!\n")
+}
+
+
